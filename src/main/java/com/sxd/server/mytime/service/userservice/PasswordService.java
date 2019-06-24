b590f85dd243860b83a88371e3ed5aa8f90a1e30
Sxd <714599564@qq.com>
2019-06-24 16:54:13 +0800

    0.2.0 unit test add dependency and modify word

--- src/main/java/com/sxd/server/mytime/service/userservice/PasswordService.java
+++ src/main/java/com/sxd/server/mytime/service/userservice/PasswordService.java
@@ -13,7 +13,7 @@ public class PasswordService {
         this.userMapper = userMapper;
     }
 
-    //修改用户密码，返回修改结果。0失败，1成功，2用户不存在
+    //修改用户密码，返回修改结果。0失败，1成功，-2用户不存在
     public int changePassword(Integer userId, String password) {
         User user = userMapper.selectByPrimaryKey(userId);
         if (user == null) {
